6bddd2e04922fc03a3eea2978699e84892b396f1
mateusz <mateusz.w.air@gmail.com>
2025-10-04 18:07:21 +0200

    add new project

---
+++ front/FrontMaui/FrontMaui/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace FrontMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "FrontMaui" };
+        }
+    }
+}
