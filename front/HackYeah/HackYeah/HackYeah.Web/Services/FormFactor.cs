47ccd6ad28fa920e6eac7b576debb4ab223d37be
mateusz <mateusz.w.air@gmail.com>
2025-10-04 17:51:07 +0200

    front

---
+++ front/HackYeah/HackYeah/HackYeah.Web/Services/FormFactor.cs
@@ -0,0 +1,17 @@
+using HackYeah.Shared.Services;
+
+namespace HackYeah.Web.Services
+{
+    public class FormFactor : IFormFactor
+    {
+        public string GetFormFactor()
+        {
+            return "Web";
+        }
+
+        public string GetPlatform()
+        {
+            return Environment.OSVersion.ToString();
+        }
+    }
+}
---
+++ front/HackYeah/HackYeah/HackYeah/MauiProgram.cs
@@ -0,0 +1,32 @@
+﻿using HackYeah.Services;
+using HackYeah.Shared.Services;
+using Microsoft.Extensions.Logging;
+
+namespace HackYeah
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            // Add device-specific services used by the HackYeah.Shared project
+            builder.Services.AddSingleton<IFormFactor, FormFactor>();
+
+            builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
