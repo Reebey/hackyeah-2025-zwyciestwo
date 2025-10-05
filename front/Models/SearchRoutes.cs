5d14daa6b6aa5fc1c06a41766e0bbfc0b00ccc9a
Peter <Peter@PETER>
2025-10-05 08:56:07 +0200

    uzycie endpojntow

---
+++ front/Models/SearchRoutes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace front.Models
+{
+    public class SearchRoutes
+    {
+        public string StartStationId { get; set; }
+        public string EndStationId { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
